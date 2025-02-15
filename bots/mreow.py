ca6dce33705660a5e88c93978bc17202919835b2
wade-cheng <wadecheng3301@gmail.com>
2025-02-15 17:37:13 -0500

    first commit

---
+++ bots/mreow.py
@@ -0,0 +1,14 @@
+from src.player import Player
+from src.map import Map
+from src.robot_controller import RobotController
+from src.game_constants import Team, Tile, GameConstants, Direction, BuildingType, UnitType
+
+from src.units import Unit
+from src.buildings import Building
+
+class BotPlayer(Player):
+    def __init__(self, map: Map):
+        pass
+    
+    def play_turn(self, rc: RobotController):
+        return
